6a24f82d7c179c9b62ecbcce27925803e0c050da
b-tima <morgan.a9904@gmail.com>
2022-10-08 14:04:48 +0200

    hej

--- parser.m
+++ parser.m
@@ -1,9 +1,14 @@
 opts = delimitedTextImportOptions("Delimiter", ',');
-data = readmatrix("2630000000_Hz.csv", opts);
-m = data(1, 2:end);
-m = str2double(m);
-top_m = m(1:1024);
-bottom_m = m(1025:2048);
-m = [bottom_m top_m];
+data = readmatrix("1820000000_Hz.csv", opts);
+num_samples = size(data);
 
-plot(m);
+for i=1:num_samples(1)
+    m = data(i, 2:end);
+    m = str2double(m);
+    top_m = m(1:1024);
+    bottom_m = m(1025:2048);
+    m = [bottom_m top_m];
+    
+    plot(m); 
+    pause;
+end
